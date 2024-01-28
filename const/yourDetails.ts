f594d827f567df86fe9423b24163dc9c94f4daaf
orange <101954606+in4r03@users.noreply.github.com>
2024-01-28 16:08:46 +0100

    Delete const directory

--- const/yourDetails.ts
+++
@@ -1,5 +0,0 @@
-// Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xb0dc05176989Fb2ba968be906089f229DBBBF821";
-
-// Replace this with your token id
-export const tokenId: string = "1";
